142593c513f79819c70a190ab15b54f070fc59a3
Irrion <mozhayko2017@mail.ru>
2020-01-13 21:38:30 +1000

    vrode rabotaet

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 struct tree_elem
 {
 	int key;
@@ -8,7 +7,6 @@ struct tree_elem
 	tree_elem*left_branch;
 	tree_elem*right_branch;
 	tree_elem*prev;
-
 	tree_elem(int key, int value, tree_elem*left = nullptr, tree_elem* previos = nullptr, tree_elem*right = nullptr)
 	{
 		this->key = key;
@@ -17,14 +15,21 @@ struct tree_elem
 		this->right_branch = right;
 		this->prev = previos;
 	}
-
 };
-class tree 
+class tree
 {
 private:
 	tree_elem* head;
-	
-public: 
+	void prin_all(tree_elem*current)
+	{
+		if (current != nullptr)
+		{
+			cout << current->value << " - " << current->key << endl;
+			prin_all(current->left_branch);
+			prin_all(current->right_branch);
+		}
+	}
+public:
 	tree()
 	{
 		head = nullptr;
@@ -43,20 +48,11 @@ public:
 		else
 		{
 			tree_elem *current = head;
-						
-			if (key == head->key)
-			{
-				current = head;
-				delete head;
-			}
-			else
-			{
 				while (current != nullptr)
 				{
 					if (current->key == key)
 					{
 						current->value = new_elem->value;
-
 						break;
 					}
 					else if (key < current->key)
@@ -100,9 +96,7 @@ public:
 				{
 					delete new_elem;
 				}
-			}
 		}
-		
 	}
 	void remove(int key)
 	{
@@ -111,7 +105,6 @@ public:
 		{
 			if (key != head->key)
 			{
-
 				while (current->key != key)
 				{
 					if (key > current->key)
@@ -140,7 +133,7 @@ public:
 					}
 				}
 				tree_elem* inher = current;
-			
+
 				if (current->right_branch != nullptr && current->left_branch != nullptr)
 				{
 					current = current->left_branch;
@@ -150,17 +143,46 @@ public:
 						{
 							current = current->right_branch;
 						}
+						if (current->left_branch != nullptr)
+						{
+							current->left_branch->prev = current->prev;
+							current->prev->right_branch = current->left_branch;
+						}
+						else
+						{
+							current->prev->right_branch = nullptr;
+						}
 						current->right_branch = inher->right_branch;
 						current->left_branch = inher->left_branch;
-						current->prev->right_branch = nullptr;
 						current->prev = inher->prev;
-						inher->prev->right_branch = current;
+						current->right_branch->prev = current;
+						current->left_branch->prev = current;
+						if (current->key > inher->prev->key)
+						{
+							inher->prev->right_branch = current;
+							
+						}
+						else if (current->key < inher->prev->key)
+						{
+							inher->prev->left_branch = current;
+						}
+						delete inher;
+						
 					}
 					else
 					{
 						current->right_branch = inher->right_branch;
 						current->prev = inher->prev;
-						inher->prev->right_branch = current;
+						current->right_branch->prev = current;
+						if (current->key > inher->prev->key)
+						{
+							inher->prev->right_branch = current;
+
+						}
+						else if (current->key < inher->prev->key)
+						{
+							inher->prev->left_branch = current;
+						}
 						delete inher;
 					}
 
@@ -181,14 +203,30 @@ public:
 				{
 					current = current->right_branch;
 					current->prev = inher->prev;
-					inher->prev->right_branch = current;
+					if (current->key > inher->prev->key)
+					{
+						inher->prev->right_branch = current;
+
+					}
+					else if (current->key < inher->prev->key)
+					{
+						inher->prev->left_branch = current;
+					}
 					delete inher;
 				}
 				else if (current->right_branch == nullptr && current->left_branch != nullptr)
 				{
 					current = current->left_branch;
 					current->prev = inher->prev;
-					inher->prev->left_branch = current;
+					if (current->key > inher->prev->key)
+					{
+						inher->prev->right_branch = current;
+
+					}
+					else if (current->key < inher->prev->key)
+					{
+						inher->prev->left_branch = current;
+					}
 					delete  inher;
 				}
 			}
@@ -207,16 +245,40 @@ public:
 						{
 							head = head->right_branch;
 						}
+						if (head->left_branch != nullptr)
+						{
+							head->left_branch->prev = head->prev;
+							head->prev->right_branch = head->left_branch;
+						}
+						else
+						{
+							head->prev->right_branch = nullptr;
+						}
 						head->right_branch = current->right_branch;
 						head->left_branch = current->left_branch;
-						head->prev->right_branch = nullptr;
 						head->prev = nullptr;
+						if (head->right_branch != nullptr)
+						{
+							head->right_branch->prev = head;
+						}
+						if (head->left_branch != nullptr)
+						{
+							head->left_branch->prev = head;
+						}
 						delete current;
 					}
 					else
 					{
 						head->right_branch = current->right_branch;
 						head->prev = nullptr;
+						if (head->right_branch != nullptr)
+						{
+							head->right_branch->prev = head;
+						}
+						if (head->left_branch != nullptr)
+						{
+							head->left_branch->prev = head;
+						}
 						delete current;
 					}
 				}
@@ -303,16 +365,40 @@ public:
 						{
 							head = head->right_branch;
 						}
+						if (head->left_branch != nullptr)
+						{
+							head->left_branch->prev = head->prev;
+							head->prev->right_branch = head->left_branch;
+						}
+						else
+						{
+							head->prev->right_branch = nullptr;
+						}
 						head->right_branch = current->right_branch;
-						head->left_branch = current->left_branch;
-						head->prev->right_branch = nullptr;
+						head->left_branch = current->left_branch;						
 						head->prev = nullptr;
+						if (head->right_branch != nullptr)
+						{
+							head->right_branch->prev = head;
+						}
+						if (head->left_branch != nullptr)
+						{
+							head->left_branch->prev = head;
+						}
 						delete current;
 					}
 					else
 					{
 						head->right_branch = current->right_branch;
 						head->prev = nullptr;
+						if (head->right_branch != nullptr)
+						{
+							head->right_branch->prev = head;
+						}
+						if (head->left_branch != nullptr)
+						{
+							head->left_branch->prev = head;
+						}
 						delete current;
 					}
 				}
@@ -335,16 +421,20 @@ public:
 			cout << "Tree is empty" << endl;
 		}
 	}
-		
+	void print_all()
+	{
+		tree_elem*current = head;
+		prin_all(current);	
+	}
 };
 
-int main()
-{
-	
+
+void old() {
+
 	tree n;
-	
+
 	n.add(100, 4);
-	n.add(50, 3);
+	n.add(100, 3);
 	n.add(163, 5);
 	n.add(40, 6);
 	n.add(60, 7);
@@ -362,9 +452,9 @@ int main()
 	n.add(171, 43);
 	n.add(209, 56);
 	n.add(211, 981);
-	
-	
-	
+
+	n.print_all();
+	cout << endl;
 	n.find(100);
 	n.find(50);
 	n.find(163);
@@ -386,8 +476,8 @@ int main()
 	n.find(211);
 	cout << "\n";
 
-	n.remove(163);
-	
+	n.remove_all();
+
 	cout << "\n";
 	n.find(100);
 	n.find(50);
@@ -408,6 +498,65 @@ int main()
 	n.find(171);
 	n.find(209);
 	n.find(211);
+	system("pause");
+}
+
+
+void test_0() {
+
+	/*
+							20
+			10							30
+	5				15			 25				35
+  3   7	          13  17	   23  27		  33  37
+	
+	*/
+	tree tr;
+	tr.add(20, 0);
+	tr.add(10, 1);
+	tr.add(5, 2);
+	tr.add(3, 3);
+	tr.add(7, 4);
+	tr.add(15, 5);
+	tr.add(13, 6);
+	tr.add(17, 7);
+	tr.add(30, 8);
+	tr.add(25, 9);
+	tr.add(23, 10);
+	tr.add(27, 11);
+	tr.add(35, 12);
+	tr.add(33, 13);
+	tr.add(37, 14);
+	tr.add(26, 35);
+	tr.add(16, 76);
+
+	tr.remove(10);
+	tr.print_all();
+}
+void test_0_1() {
+
+	/*
+							20
+			10							30
+	5				15			 25				35
+
+	*/
+	tree tr;
+	tr.add(20, 0);
+	tr.add(10, 1);
+	tr.add(5, 2);
+	tr.add(15, 5);
+	tr.add(30, 8);
+	tr.add(25, 9);
+	tr.add(35, 12);
+
+	tr.remove(30);
+	tr.print_all();
+}
+
+int main()
+{
+	test_0();
 	system("pause");
 	return 0;
 }
